75679a1d404f6bf08a94b30949028cff483af7d3
Ekanshh <ekanshh.sharma@gmail.com>
2020-11-16 11:01:13 +0530

    Added functions - stringfy_list, multiply_list, get_highest _value, get_lowest_value

--- list_manager.py
+++ list_manager.py
@@ -18,17 +18,24 @@ def reverse_order(list):
 
 def stringfy_list(list):
     '''returns a list with all elements turned into strings'''
-    pass
+    list_1 = ' '.join([str(elem) for elem in list]) 
+    return list_1
 
 def multiply_list(list, multiple):
-    '''returns the list with all elements multipled by the value multiple'''
-    pass
+    '''returns the list with all elements multipled by the value multiple'''    
+    list_1 = [ i*multiple for i in list]
+    return list_1
+    
 
 def get_highest_value(list):
-    '''returns the highest value of the list'''
-    pass
+    return max(list)
 
 def get_lowest_value(list):
-    '''returns the lowest value of the list'''
-    pass
+    return min(list)
+    
 
+list_1 = [1, 2, 3, 4]
+print(stringfy_list(list_1))
+print(multiply_list(list_1,5))
+print(get_highest_value(list_1))
+print(get_lowest_value(list_1))
