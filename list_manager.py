26560930b4cc78a6b8138efef7d7d6a0d17798af
Musharraf <alisyedmusharraf1995@gmail.com>
2020-11-16 10:41:43 +0500

    Ekansh functions modified and output shown

--- list_manager.py
+++ list_manager.py
@@ -21,26 +21,31 @@ def reverse_order(list1):
     return list1[::-1]
 
 
-def stringfy_list(list):
+def stringfy_list(list1):
     '''returns a list with all elements turned into strings'''
-    list_1 = ' '.join([str(elem) for elem in list]) 
+    list_1 = ' '.join([str(elem) for elem in list1]) 
     return list_1
 
-def multiply_list(list, multiple):
+def multiply_list(list1, multiple):
     '''returns the list with all elements multipled by the value multiple'''    
-    list_1 = [ i*multiple for i in list]
+    list_1 = [ i*multiple for i in list1]
     return list_1
     
 
-def get_highest_value(list):
-    return max(list)
+def get_highest_value(list1):
+    return max(list1)
 
-def get_lowest_value(list):
-    return min(list)
+def get_lowest_value(list1):
+    return min(list1)
     
 
-list_1 = [1, 2, 3, 4]
-print(stringfy_list(list_1))
-print(multiply_list(list_1,5))
-print(get_highest_value(list_1))
-print(get_lowest_value(list_1))
+if __name__ == "__main__":
+    list_1 = [1, 2, 3, 4]
+    print(stringfy_list(list_1))
+    print(multiply_list(list_1,5))
+    print(get_highest_value(list_1))
+    print(get_lowest_value(list_1))
+    print(random_order(list_1))
+    print(order_by_increasing_value(list_1))
+    print(order_by_decreasing_value(list_1))
+    print(reverse_order(list_1))
